ae7fdc663b3a3ca637f2d7d87354a549905880e2
djc06048 <gsafe1213@gmail.com>
2022-12-18 02:30:19 +0900

    spring boot로 hello world 출력하기

---
+++ practice1/src/main/java/com/example/practice1/hello/hello.java
@@ -0,0 +1,10 @@
+package com.example.practice1.hello;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class hello {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello world";
+    }
+}
